0b1fd63b20b4a87ebcf143d305e274cf999811c6
Vladislav-Ciuperca <ciuperca37@gmail.com>
2024-04-18 22:52:09 +0200

    commit-1

---
+++ JavaScript/myscript.js
@@ -0,0 +1,26 @@
+function calcPrice() {
+    let anni = document.getElementById("anni").value;
+    let distance = document.getElementById("distanza").value;
+    let baseprice = distance * 0.21;
+
+    if (anni == 0) {
+        let fixedprice = baseprice.toFixed(2)
+        document.getElementById("demo").innerHTML = "il costo del tuo viaggio è di " + fixedprice + " + iva";
+    }
+    else if (anni < 18) {
+        const finalprice = baseprice * 0.8
+        let fixedprice = finalprice.toFixed(2)
+        document.getElementById("demo").innerHTML = "il costo del tuo viaggio è di " + fixedprice + " + iva";
+    }
+    else if (anni >= 65) {
+        const finalprice = baseprice * 0.6
+        let fixedprice = finalprice.toFixed(2)
+        document.getElementById("demo").innerHTML = "il costo del tuo viaggio è di " + fixedprice + " + iva";
+    }
+    else {
+        let fixedprice = baseprice.toFixed(2)
+        document.getElementById("demo").innerHTML = "il costo del tuo viaggio è di " + fixedprice + " + iva";
+    }
+}
+function financial(x) {
+}
