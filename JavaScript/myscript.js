0ce896d4f338e737e5e1f241d321a8038f9cd15d
Vladislav-Ciuperca <ciuperca37@gmail.com>
2024-04-20 15:08:34 +0200

    last commit

--- JavaScript/myscript.js
+++ JavaScript/myscript.js
@@ -1,26 +1,40 @@
+
+
 function azione() {
     let anni = document.getElementById("anni").value;
     let distance = document.getElementById("distanza").value;
     let baseprice = distance * 0.21;
+    let nome = document.getElementById("nome").value;
+    document.getElementById("passeggero").innerHTML = nome;
 
     if (anni == 0) {
         let fixedprice = baseprice.toFixed(2)
-        document.getElementById("demo").innerHTML = "il costo del tuo viaggio è di " + fixedprice + " + iva";
+        document.getElementById("prezzo").innerHTML = fixedprice + '€';
+        document.getElementById("carrozzan").innerHTML = Math.floor(Math.random() * 100) + 1;
+        document.getElementById("codice").innerHTML = Math.floor(Math.random() * 60000) + 600000;
     }
+
     else if (anni < 18) {
         const finalprice = baseprice * 0.8
         let fixedprice = finalprice.toFixed(2)
-        document.getElementById("demo").innerHTML = "il costo del tuo viaggio è di " + fixedprice + " + iva";
+        document.getElementById("prezzo").innerHTML = fixedprice + '€';
+        document.getElementById("sconto").innerHTML = '20%';
+        document.getElementById("carrozzan").innerHTML = Math.floor(Math.random() * 100) + 1;
+        document.getElementById("codice").innerHTML = Math.floor(Math.random() * 60000) + 600000;
+
     }
     else if (anni >= 65) {
         const finalprice = baseprice * 0.6
         let fixedprice = finalprice.toFixed(2)
-        document.getElementById("demo").innerHTML = "il costo del tuo viaggio è di " + fixedprice + " + iva";
+        document.getElementById("prezzo").innerHTML = fixedprice + '€';
+        document.getElementById("sconto").innerHTML = '40%';
+        document.getElementById("carrozzan").innerHTML = Math.floor(Math.random() * 100) + 1;
+        document.getElementById("codice").innerHTML = Math.floor(Math.random() * 60000) + 600000;
     }
     else {
         let fixedprice = baseprice.toFixed(2)
-        document.getElementById("demo").innerHTML = "il costo del tuo viaggio è di " + fixedprice + " + iva";
+        document.getElementById("prezzo").innerHTML = fixedprice + '€';
+        document.getElementById("carrozzan").innerHTML = Math.floor(Math.random() * 100) + 1;
+        document.getElementById("codice").innerHTML = Math.floor(Math.random() * 60000) + 600000;
     }
-}
-function financial(x) {
 }
